b3a9aa7977ed639dd279007d3a922d160e174152
ishami-i <i.irene@alustudent.com>
2026-09-02 03:26:47 -0700

    created 4-puts.c file

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: success on execution
+ */
+int main(void)
+{
+	/*  Print programming is message */
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
