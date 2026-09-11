2b3bf82177c143ca358d78b6d939ffea83a85483
ISHAMI Irené <i.irene@alustudent.com>
2026-09-11 19:37:01 +0200

    Fix printf format string for last digit output

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,9 +15,9 @@ int main(void)
 	if(last_digit > 5) {
 		printf("Last digit of %d is greater than 5", n);
 	} else if(last_digit == 0) {
-		printf("Last digit of %d and is 0");
+		printf("Last digit of %d and is 0", n);
 	} else {
-		printf("Last digit of %d and is less than 6 and not 0");
+		printf("Last digit of %d and is less than 6 and not 0", n);
 	}
 	return (0);
 }
