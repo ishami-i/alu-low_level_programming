b592749617ffd9244860c0f1dc8ce34e28aabbc3
ISHAMI Irené <i.irene@alustudent.com>
2026-09-11 19:41:10 +0200

    Improve output messages for last digit evaluation
    Refactor output messages to include last digit value.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,24 +1,36 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and evaluates its value.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
-	int last_digit;
-	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	last_digit = n % 10;
-	if(last_digit > 5) {
-		printf("Last digit of %d is greater than 5", n);
-	} else if(last_digit == 0) {
-		printf("Last digit of %d and is 0", n);
-	} else {
-		printf("Last digit of %d and is less than 6 and not 0", n);
-	}
-	return (0);
+    int n;
+    int last_digit;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    /* Get the last digit */
+    last_digit = n % 10;
+
+    /* Print output based on the value of the last digit */
+    if (last_digit > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+    }
+    else if (last_digit == 0)
+    {
+        printf("Last digit of %d is %d and is 0\n", n, last_digit);
+    }
+    else
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+    }
+
+    return (0);
 }
