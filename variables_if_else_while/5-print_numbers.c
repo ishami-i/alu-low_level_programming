44f8998eb7461d0522fcf20b9910e5692a16e0bb
ISHAMI Irené <i.irene@alustudent.com>
2026-09-11 23:00:29 +0200

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	int i;
-	for(i = 0; i < 10; i++) 
+
+	for (i = 0; i < 10; i++)
+	{
 		printf("%d", i);
+	}
 
 	return (0);
 }
