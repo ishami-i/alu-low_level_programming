77379d68b6d1b7c6833bdf4c9b862ca1af3fb492
ishami-i <i.irene@alustudent.com>
2026-09-11 13:48:55 -0700

    code to print letters excluding q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase except q and e followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++) {
+		if(c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
