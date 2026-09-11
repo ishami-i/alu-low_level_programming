493915f40fdb2856c53cdc436f23710cc7d88e8f
ishami-i <i.irene@alustudent.com>
2026-09-11 14:25:28 -0700

    added and created  combo file

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        putchar(i + '0'); // Convert integer to its character equivalent
+
+        if (i < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n'); // Add a new line at the end
+
+    return (0);
+}
+
