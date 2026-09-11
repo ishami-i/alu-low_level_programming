0377423de65baeafe2b552477f8f5178de8b4acd
ISHAMI Irené <i.irene@alustudent.com>
2026-09-11 23:31:23 +0200

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,21 +6,20 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0'); // Convert integer to its character equivalent
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0'); /* Convert integer to its character equivalent */
 
-        if (i < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
-    putchar('\n'); // Add a new line at the end
+	putchar('\n'); /* Add a new line at the end */
 
-    return (0);
+	return (0);
 }
-
