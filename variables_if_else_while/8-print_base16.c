1b22dbcc7805e8f87727c1617313a458795da8c8
ishami-i <i.irene@alustudent.com>
2026-09-11 14:14:15 -0700

    created print in base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase and bigcase followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int b;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (b = 1; c <= ; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
