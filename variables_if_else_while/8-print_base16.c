0ca3f029cbb724618780932fea0d5924f0e0d886
ISHAMI Irené <i.irene@alustudent.com>
2026-09-11 23:27:42 +0200

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
- * main -  prints all the numbers of base 16 in lowercase, followed by a new line.
+ * main - prints all the numbers of base 16 in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int c;
+	int i; /* Declare i here to comply with C89/C90 standards */
 	
 	/* Loop from character '0' to character '9' */
-	for (int i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 		putchar(i);
 
 	/* Loop from character 'a' to character 'f' */
