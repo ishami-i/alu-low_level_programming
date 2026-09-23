c92b179b6a3d46b33c0949bcad33495dc75852f1
ishami-i <i.irene@alustudent.com>
2026-09-22 22:45:12 -0700

    add the print alphabet 10X

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter); /* First _putchar call */
+	}
+	_putchar('\n'); /* Second _putchar call */
+}
