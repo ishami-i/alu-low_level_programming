e09afd5c32a2c83af531359a11c2fa5a1dc53885
ISHAMI Irené <i.irene@alustudent.com>
2026-09-23 07:23:41 +0200

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 
-int main() {
-	printf("_putchar\n");
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str = "_putchar\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
 	return (0);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
